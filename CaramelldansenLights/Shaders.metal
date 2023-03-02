41d42ac934aa4ca3d2944db8583bc8451ee97dcd
Aleksandr Shepelenok <aleksandr.shepelenok@itechart-group.com>
2023-03-02 13:24:46 +0300

    Session 2: Animations. Rotating a square.
    - Drawing triangle strips
    - Vertex colors
    - Scales. Handling viewport
    - Model-View and Projection matrices
    - Animations

--- CaramelldansenLights/Shaders.metal
+++ CaramelldansenLights/Shaders.metal
@@ -10,20 +10,30 @@ using namespace metal;
 
 struct VertexIn {
     float2 position [[attribute(0)]];
+    float3 color [[attribute(1)]];
 };
 
 struct VertexOut {
     float4 position [[position]];
+    float4 color;
+};
+
+struct Uniforms {
+    float2 viewportSize;
+    float4x4 modelViewMatrix;
 };
 
 vertex VertexOut vertex_main(uint vertexID [[vertex_id]],
-                             constant VertexIn *vertices [[buffer(0)]]) {
+                             constant VertexIn *vertices [[buffer(0)]],
+                             constant Uniforms &uniforms [[buffer(1)]]) {
     VertexOut out;
-    out.position = vector_float4(0, 0, 0, 1);
-    out.position.xy = vertices[vertexID].position.xy;
+    float4 rotatedPos = uniforms.modelViewMatrix * float4(vertices[vertexID].position.xy, 0, 1);
+    float2 position = rotatedPos.xy / (uniforms.viewportSize / 2.0);
+    out.position = float4(position, 0, 1);
+    out.color = float4(vertices[vertexID].color, 1);
     return out;
 }
 
 fragment float4 fragment_main(VertexOut in [[stage_in]]) {
-    return float4(1, 0, 0, 1);
+    return in.color;
 }
--- CaramelldansenLights/ViewController.swift
+++ CaramelldansenLights/ViewController.swift
@@ -40,6 +40,12 @@ class ViewController: UIViewController {
         return Renderer(device: device, commandQueue: queue, pipelineState: pipelineState)
     }()
 
+    lazy var uniforms: Uniforms = {
+        Uniforms(viewportSize: viewportSize(with: mtkView.drawableSize), modelViewMatrix: matrix_identity_float4x4)
+    }()
+
+    var angle: Float = 0.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -47,24 +53,45 @@ class ViewController: UIViewController {
         mtkView.device = renderer.device
     }
 
+    private func viewportSize(with size: CGSize) -> simd_float2 {
+        simd_float2(Float(size.width), Float(size.height))
+    }
+
 }
 
 struct Vertex {
-    let position: vector_float2
+    let position: simd_float2
+    let color: simd_float3
+}
+
+struct Uniforms {
+    var viewportSize: simd_float2
+    var modelViewMatrix: simd_float4x4
 }
 
 extension ViewController: MTKViewDelegate {
 
-    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        uniforms.viewportSize = viewportSize(with: size)
+    }
 
     func draw(in view: MTKView) {
-        
+        angle += 1
+        if angle >= 360 {
+            angle = 0
+        }
+        let radians = angle * .pi / 180.0
+        let rotationQuaternion = simd_quatf(angle: radians, axis: simd_float3(0, 0, 1))
+        let rotationMatrix = simd_float4x4(rotationQuaternion)
+        uniforms.modelViewMatrix = rotationMatrix
+
         guard let currentDrawable = view.currentDrawable else { return }
 
         let vertices = [
-            Vertex(position: [-1, 1]),
-            Vertex(position: [1, 1]),
-            Vertex(position: [-1, -1])
+            Vertex(position: [-250, 250], color: [1, 0, 0]),
+            Vertex(position: [250, 250], color: [1, 1, 0]),
+            Vertex(position: [-250, -250], color: [0, 1, 1]),
+            Vertex(position: [250, -250], color: [0, 1, 0])
         ]
 
         guard
@@ -73,14 +100,19 @@ extension ViewController: MTKViewDelegate {
             let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor),
             let vertexBuffer = renderer.device.makeBuffer(bytes: vertices,
                                                           length: vertices.count * MemoryLayout<Vertex>.stride,
-                                                          options: []) else {
+                                                          options: []),
+            let uniformsBuffer = renderer.device.makeBuffer(bytes: [uniforms],
+                                                            length: MemoryLayout<Uniforms>.stride,
+                                                            options: [])
+        else {
             print("Failed to draw vertices")
             return
         }
 
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderEncoder.setVertexBuffer(uniformsBuffer, offset: 0, index: 1)
         renderEncoder.setRenderPipelineState(renderer.pipelineState)
-        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
+        renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: vertices.count)
         renderEncoder.endEncoding()
 
         commandBuffer.present(currentDrawable)
